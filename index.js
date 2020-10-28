5cbd2367dfe48fb57d5a7c01525f52a30009f94f
mabvuto sichone <mabvutosicho@gmail.com>
2020-10-28 06:33:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+let canv = document.querySelector("canvas");
+
+canv.width = window.windth;
+canv.height = window.height;
+let cont = canv.getContext("2d");
+
+
+cont.fillRect(100,100,100,100);
